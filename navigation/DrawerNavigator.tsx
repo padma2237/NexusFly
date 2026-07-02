ac21885bb8bcd8f48bf2a8cc4c209a666f60636f
padma2237 <padmarampegu2237@gmail.com>
2026-07-02 01:15:32 +0000

    Add navigation drawer

---
+++ navigation/DrawerNavigator.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import ChatScreen from "../screens/ChatScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerNavigator() {
+  return (
+      <Drawer.Navigator
+      id="MainDrawer"
+            initialRouteName="Chat"
+                  screenOptions={{
+                          headerShown: false,
+                                }}
+                                    >
+                                          <Drawer.Screen
+                                                  name="Chat"
+                                                          component={ChatScreen}
+                                                                />
+
+                                                                      <Drawer.Screen
+                                                                              name="Settings"
+                                                                                      component={SettingsScreen}
+                                                                                            />
+                                                                                                </Drawer.Navigator>
+                                                                                                  );
+                                                                                                  }
